e0a886844be7d38b26ec10fff6761079f39c509b
Stefan Mueller <mu.stefan@googlemail.com>
2020-03-28 12:56:34 +0100

    Refactored code

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var async = require('ep_etherpad-lite/node_modules/async');
 var padManager = require('ep_etherpad-lite/node/db/PadManager');
+// use Node.js PRNG to generate a "unique" sequence
+const crypto = require('crypto');
 
 exports.registerRoute = function (hook_name, args, cb) {
-  args.app.get('/randomPad', function(req, res) {
-    var padName;
-    
-    createRandomPadName = function() {
+  args.app.get('/randomPad', function (req, res) {
+
+    createRandomPadName = function () {
       // the number of distinct chars (64) is chosen to ensure that
       // the selection will be uniform when using the PRNG below
       var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_";
@@ -14,27 +15,25 @@ exports.registerRoute = function (hook_name, args, cb) {
       var string_length = 20;
       // make room for 8-bit integer values that span from 0 to 255.
       var randomarray = new Uint8Array(string_length);
-      // use Node.js PRNG to generate a "unique" sequence
-      const crypto = require('crypto');
+
       crypto.randomFillSync(randomarray);
       var randomstring = '';
-      for (var i = 0; i < string_length; i++)
-      {
+      for (var i = 0; i < string_length; i++) {
         // instead of writing "Math.floor(randomarray[i]/256*64)"
         // we can save some cycles.
-        var rnum = Math.floor(randomarray[i]/4);
+        var rnum = Math.floor(randomarray[i] / 4);
         randomstring += chars.substring(rnum, rnum + 1);
       }
       return randomstring;
     };
 
-  	async.series([
+    async.series([
       function (callback) {
         // find a really new pad
-        padName = createRandomPadName();
+        var padName = createRandomPadName();
         // redirect to new pad
         res.writeHead(302, {
-          'Location': 'p/'+padName
+          'Location': 'p/' + padName
         });
         res.end();
         callback();
